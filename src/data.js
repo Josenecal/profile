8ccaa66f19da320513517a142bb673c5155619c8
Joseph Senecal <jasenecal2@gmail.com>
2024-05-23 10:13:06 -0600

    Create data file with mock tile data

---
+++ src/data.js
@@ -0,0 +1,13 @@
+const portfolioProjectsData = [
+    {
+        'title': 'Test Project Title',
+        'description': 'Here\'s a short descrtiption of a theoretical project that I\'m going to use to test how a descritpion renders and whether or not it will cut off at the right point.',
+        'gitHubURL': '#',
+    },
+    {
+        'title': 'Second Project Title',
+        'description': 'Here is another description to test how a shorter one renders',
+        'gitHubUrl': 'https://www.github.com',
+        'deployedURL': 'https://www.google.com',
+    }
+]
