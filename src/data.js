b947b5e056a332e42351827e46757a64adea8d54
Joseph Senecal <jasenecal2@gmail.com>
2024-05-27 10:21:42 -0600

    Update skill and profile data

--- src/data.js
+++ src/data.js
@@ -1,13 +1,102 @@
 const portfolioProjectsData = [
     {
-        'title': 'Test Project Title',
-        'description': 'Here\'s a short descrtiption of a theoretical project that I\'m going to use to test how a descritpion renders and whether or not it will cut off at the right point.',
-        'gitHubURL': '#',
+        "title": "GitHub Profile",
+        "description": "A more personalized profile page hosted by GitHub Pages. This was a first attempt at building a really simple front end using JavaScript and responsive design techniques to build a simple, beautiful, dynamic profile page.",
+        "gitHubURL": "https://github.com/Josenecal/profile",
+        "deployedURL": "https://josenecal.github.io/profile",
     },
     {
-        'title': 'Second Project Title',
-        'description': 'Here is another description to test how a shorter one renders',
-        'gitHubUrl': 'https://www.github.com',
-        'deployedURL': 'https://www.google.com',
-    }
+        "title": "Recipe Cache",
+        "description": "This project was meant to be an academic opportunity to look more in-depth at the advanced features of the Ruby on Rails framework, as well as a reintroduction to JavaScript and front-end technologies.",
+        "gitHubURL": "https://github.com/Josenecal/recipe-cache",
+        "deployedURL": "https://recipe-cache.fly.dev",
+    },
+    {
+        "title": "Dungeon Ball",
+        "description": "An early experiment in microservice oriented deployment architecture, the Dungeon Ball aplication used publically available API data for football players to algorythmically generate a Dungeons and Dragons style character sheet.",
+        "gitHubURL": "https://github.com/Dungeon-ball",
+        "deployedURL": "",
+    },
+    {
+        "title": "Pup Trainer",
+        "description": "A collaborative project with Turing's Front End students, Pup Trainer was meant to be an all-in-one gamified solution for trainig your new best friend, complete with helpful instructions and how-to's. It was also a first experiment with GraphQL!",
+        "gitHubURL": "https://github.com/PupTrainer",
+        "deployedURL": "",
+    },
+    {
+        "title":"Sweater Weather",
+        "description":"This project was an early API-only rails project with 5 endpoints. The application itself synthisized informoation from other publically available API's to generate JSON responses for things like roadtrip weather predictions and location-based weather forcasts.",
+        "gitHubURL": "https://github.com/Josenecal/sweater_wheather",
+        "deployedURL": "",
+    },
+    {
+        "title":"RomCom",
+        "description":"An exercise in JavaScript pilfered from Turing's Front End curriculum, this fun little front-end application generates movie covers with random cover art, titles and descriptions, and lets you save and revisit your favorites!",
+        "gitHubURL": "https://github.com/Josenecal/romcom",
+        "deployedURL": "",
+    },
+    {
+        "title":"JavaScript Foundations",
+        "description":"Also pilfered from Turing's Front End Curriculum, this master problem set is meant to be an introduction to both good JavaScript convention and TDD strategies",
+        "gitHubURL": "https://github.com/Josenecal/javascript-foundations",
+        "deployedURL": "",
+    },
+    // {
+    //     "title":"",
+    //     "description":"",
+    //     "gitHubURL": "",
+    //     "deployedURL": "",
+    // },
+    // {
+    //     "title":"",
+    //     "description":"",
+    //     "gitHubURL": "",
+    //     "deployedURL": "",
+    // }
+]
+
+var skills = [
+    // {
+    //     "image": "",
+    //     "width": "",
+    //     "height": "",
+    //     "url": "",
+    // },
+    {
+        "image": "images/css.jpg",
+        "width": "50px",
+        "height": "50px",
+    },{
+        "image": "images/html.jpg",
+        "width": "50px",
+        "height": "50px",
+    },{
+        "image": "images/java_script.svg",
+        "width": "50px",
+        "height": "50px",
+    },{
+        "image": "images/postgresql.png",
+        "width": "50px",
+        "height": "50px",
+    },{
+        "image": "images/rails.png",
+        "width": "50px",
+        "height": "50px",
+    },{
+        "image": "images/redis.png",
+        "width": "50px",
+        "height": "50px",
+    },{
+        "image": "images/rspec.png",
+        "width": "50px",
+        "height": "50px",
+    },{
+        "image": "images/ruby.png",
+        "width": "50px",
+        "height": "50px",
+    },{
+        "image": "images/sidekiq.png",
+        "width": "50px",
+        "height": "50px",
+    },
 ]
